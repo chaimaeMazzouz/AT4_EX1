e40c0b4a306b57ce8bb9058cc73e9efd235c3487
chaimaeMazzouz <mazzouz.chaimae.dev@gmail.com>
2020-11-06 22:28:53 +0100

    TP for school using c#

---
+++ AT4_EX1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AT4_EX1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection(@"data source =DESKTOP-7TC9CTO\SQLEXPRESS;database=VolAvion;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader read;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT Av, Marque, TypeAvion FROM avion";
+            try
+            {
+                dataGridView1.Rows.Clear();
+                con.Open();
+                read = cmd.ExecuteReader();
+                while (read.Read())
+                {
+                    dataGridView1.Rows.Add(read["Av"], read["Marque"], read["TypeAvion"]);
+                }
+            }
+            catch(Exception exp)
+            {
+                MessageBox.Show("Erreur levée :" +exp.Message);
+            }
+            finally
+            {
+                read.Close();
+                con.Close();
+            }
+        }
+    }
+}
